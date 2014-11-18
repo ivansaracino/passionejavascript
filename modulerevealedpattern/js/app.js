938bb882ea47a1bfba98dcf7a5e93335d982312e
Ivan Saracino <ivan.saracino@gmail.com>
2014-11-18 16:27:51 +0100

    modifica toggle dei bottoni

--- modulerevealedpattern/js/app.js
+++ modulerevealedpattern/js/app.js
@@ -7,16 +7,19 @@ var modulo = (function(){
 				   		counter++;
 				   		window.document.getElementById('contenuto').innerHTML='<b>' + counter + '<b/>';
 				   	}, 1000);
-				   	document.getElementById('stop').disabled = false; 
-				   	document.getElementById('start').disabled = true; 
+				   	alternaBottoni();
 
 	               },
 	    counter = 0,
 	    stop = function() {
 	    	window.clearInterval(timer);
 	    	counter = 0;
-	    	document.getElementById('start').disabled = false; 
-	    	document.getElementById('stop').disabled = true; 
+	    	alternaBottoni();
+	    },
+	    alternaBottoni = function() {
+	    	document.getElementById('start').disabled = !document.getElementById('start').disabled;
+	    	document.getElementById('stop').disabled = !document.getElementById('start').disabled;
+
 	    };
 
 
