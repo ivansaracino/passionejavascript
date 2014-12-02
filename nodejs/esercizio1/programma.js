7c145c1d823dc766a93f9a6dd2d9d268a20edcbb
Ivan Saracino <ivan.saracino@gmail.com>
2014-12-02 10:40:34 +0100

    Esercizio 1

---
+++ nodejs/esercizio1/programma.js
@@ -0,0 +1,17 @@
+var somma = 0,
+    i = 0;
+
+if (process.argv.length > 2) {
+	for (i = 2; i < process.argv.length; i++) {
+		if (Number(process.argv[i])) {
+		  somma += parseInt(process.argv[i]);
+		}
+		else {
+		  console.log("Parametro " + process.argv[i] + " non è un numero!" );
+		}
+	} 
+	console.log(somma);
+} else {
+	console.log('Esempio di utilizzo : node programma.js 1 3 5');
+}
+
