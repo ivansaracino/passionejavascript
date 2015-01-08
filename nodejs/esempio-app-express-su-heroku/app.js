9ace07b94ba63a552888e5ce8e98ea1e9b35998f
Ivan Saracino <ivan.saracino@gmail.com>
2015-01-08 16:02:07 +0100

    biblioteca online: prima iterazione

--- nodejs/esempio-app-express-su-heroku/app.js
+++ nodejs/esempio-app-express-su-heroku/app.js
@@ -8,43 +8,24 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(cookieParser());
+
 app.use(expressSession({
 	secret: 'stringaqualunque',
 	resave: false,
 	saveUninitialized: true    
-}));  
+}));
+
 app.use(function(req,res,next) {
 	res.locals.session = req.session;
-	next(); 
+	next();     
 }); 
+
 app.use(express.static(__dirname + '/public'));  
 
 app.get('/', function(req,res) {
 	res.render('index');
-});    
-
-app.get('/benvenuto', function(req,res){
-	req.session.utente = req.query.nome;
-	res.render('benvenuto', {nomeCognome : req.query.nome});
-});      
- 
-app.get('/pagina1', function(req,res) {
-	res.render('pagina1', {libri: [{
-		titolo: 'Il signore degli anelli',
-		autore: 'Tolkien'
-	},{
-		titolo: 'Guerra e pace',
-		autore: 'Tolstoj'
-	}, {
-		titolo: 'Javascript the good parts',
-		autore: 'Crockford'
-	}]}); 
 });  
 
-app.get('/pagina2', function(req,res) {
-	res.render('pagina2');
-});        
-  
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });                 	        
