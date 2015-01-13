9085b2b4bc4b0a50a563b93ecfcde1516b23df3d
Ivan Saracino <ivan.saracino@gmail.com>
2015-01-13 17:21:49 +0100

    validazione forms

--- nodejs/esempio-app-express-su-heroku/routes/utenti.js
+++ nodejs/esempio-app-express-su-heroku/routes/utenti.js
@@ -1,27 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var serviziBiblioteca = require('../servizi/serviziUtenze');
+var form = require('express-form');
+var field = form.field;
+
+var validatoreLogin = form(
+		field('username').trim().required('', 'Inserire il nome utente!'),
+		field('password').trim().required('','Inserire la password!')
+	);
+
+	var validatoreRegistrazione = form(
+			field('username').trim().required('', 'Inserire il nome utente!'),
+			field('password').trim().required('','Inserire la password!'),
+			field('email').trim().required('','Inserire la mail').isEmail('Inserire un indirizzo di mail valido!')
+		);
 
 router.route('/login')
 	.get(function(request,response) {
-
 		response.render('login');
 	})
-	.post(function(request,response){
-		var utente = serviziBiblioteca.login(
-			request.body.username,
-			request.body.password
-		);
-		if (utente) {
-			request.session.utente = utente;	
-			response.render('home');
+	.post(validatoreLogin, function(request,response){
+		var utente = undefined;
+		if (!request.form.isValid) {
+			console.log(request.form.errors);
+			response.render('login', {errors: request.form.errors});
 		}
 		else {
-			response.render(
-				'login', 
-				{ loginFallito : 'Utente non abilitato'}
+			utente = serviziBiblioteca.login(
+				request.body.username,
+				request.body.password
 			);
+			if (utente) {
+				request.session.utente = utente;	
+				response.render('home');
+			}
+			else {
+				response.render(
+					'login', 
+					{ loginFallito : 'Utente non abilitato'}
+				);
+			}	
 		}
+		
 		
 	});
 
@@ -29,15 +49,20 @@ router.route('/registrazione')
 	.get(function(request,response) {
 		response.render('registrazione');
 	})
-	.post(function(request,response) {
+	.post(validatoreRegistrazione, function(request,response) {
 		var utente = {};
-		utente.username = request.body.username;
-		utente.password = request.body.password;
-		utente.email = request.body.email;
-		utente.profilo = 'semplice';
-		serviziBiblioteca.registra(utente);
-		response.render('login', {
-			registrazioneOk: 'Ora puoi effettuare il login!'});
+		if (!request.form.isValid) {
+			response.render('registrazione', {errors: request.form.errors})
+		} else {
+			utente.username = request.body.username;
+			utente.password = request.body.password;
+			utente.email = request.body.email;
+			utente.profilo = 'semplice';
+			serviziBiblioteca.registra(utente);
+			response.render('login', {
+				registrazioneOk: 'Ora puoi effettuare il login!'});
+		}
+		
 	});
 
 router.route('/logout')
